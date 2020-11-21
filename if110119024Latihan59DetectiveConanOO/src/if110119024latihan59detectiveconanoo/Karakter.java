d14f11566922c20cc6a78d7f4f23ab763ae84c28
kusnandi <72843119+kusnandi@users.noreply.github.com>
2020-11-22 02:25:30 +0700

    Add files via upload

---
+++ if110119024Latihan59DetectiveConanOO/src/if110119024latihan59detectiveconanoo/Karakter.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if110119024latihan59detectiveconanoo;
+
+/**
+ *
+ * @author acer
+ * KUSNANDI PIRMANSYAH
+ * IF-1
+ * 10119024
+ */
+public class Karakter extends Manusia {
+    private String role, kepribadian;
+    
+    public String getRole() {
+        return this.role;
+    }
+    
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    public String getKepribadian() {
+        return this.kepribadian;
+    }
+    
+    public void setKepribadian(String kepribadian) {
+        this.kepribadian = kepribadian;
+    }
+    
+    public void tampilKarakter() {
+        System.out.println("Nama\t\t: " + super.getNama());
+        System.out.println("Umur\t\t: " + super.getUmur());
+        System.out.println("Kepribadian\t: " + getKepribadian());
+        System.out.println("Role\t\t: " + getRole());
+        System.out.println();
+    }
+    
+}
---
+++ if110119024Latihan59DetectiveConanOO/src/if110119024latihan59detectiveconanoo/Manusia.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if110119024latihan59detectiveconanoo;
+
+/**
+ *
+ * @author acer
+ * KUSNANDI PIRMANSYAH
+ * IF-1
+ * 10119024
+ */
+public class Manusia {
+    
+    protected String nama;
+    protected int umur;
+    
+    public String getNama() {
+        return this.nama;
+    }
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    public int getUmur() {
+        return this.umur;
+    }
+    
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+    
+}
